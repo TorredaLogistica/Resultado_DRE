f9d1e19472110ce4aa91f10a2abe070166173ff2
TorredaLogistica <logistica.armazenagem.claro@gmail.com>
2026-04-02 19:49:02 -0300

    Update dashboard_dre.py

--- dashboard_dre.py
+++ dashboard_dre.py
@@ -240,11 +240,7 @@ else:
     st.plotly_chart(fig, use_container_width=True)
 
     tabela = mensal.pivot(index="tipo", columns="mes_nome", values="valor").reindex(columns=ORDEM_MESES)
-    
-st.dataframe(
-    tabela.style.format(fmt_mi),
-    use_container_width=True
-)
+    st.dataframe(tabela.applymap(fmt_mi), use_container_width=True)
 
 # ======================================================
 # GASTOS REALIZADOS
